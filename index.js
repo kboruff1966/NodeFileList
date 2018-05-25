4f26dbb1e8d938a7b6bcc69ee4eff5353af31112
Keith Boruff <kboruff@optonline.net>
2018-05-25 14:31:24 -0400

    added file format

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 
 const fs = require('fs');
 
+const createFileDataObj = (name, isDir, isFile, files) => {
+  return { name: name, isDir: isDir, isFile: isFile, files: files };
+}
+
 const program = require('commander')
   .usage('[options] [file]')
   .option('-a, --all', 'List all')
@@ -10,26 +14,64 @@ const program = require('commander')
   .option('-F, --decorator', 'displays file decorators: /, * or @')
   .parse(process.argv);
 
-if (program.all) console.log('list all files in directory: -a');
-if (program.long) console.log('long list format of files: -l');
-if (program.decorator) console.log('adorn file decorators on files: -F');
+
+
+// hack to add file argument if empty
+// TODO: I'd like to be able to just incorporate this into the command line processing above
+if (program.args.length == 0) {
+  program.args.push('./');
+}
 
 let fileList = [];
 
-// processes file list to print
+// print the processed file list according to options provided
 const callback = () => {
-  console.log("callback called. File list completed");
-  console.log(fileList);
+
+  if (program.all) console.log('list all files in directory: -a');
+  if (program.long) console.log('long list format of files: -l');
+  if (program.decorator) console.log('adorn file decorators on files: -F');
+
+
+
+
+
+  for (const fileArg of fileList) {
+
+    console.log();
+
+    if (fileArg.isDir) {
+
+      console.log(fileArg.name + ':');
+
+      // list directory contents
+      for (const f of fileArg.files) {
+
+        // bypass hidden files if -a not set
+        if (!program.all && f.search(/\..*$/) === 0) {
+          continue;
+        }
+
+
+        console.log(f);
+      }
+    }
+
+    else if (fileArg.isFile) {
+      console.log(fileArg.name);
+
+    }
+
+    else {
+      console.log(fileArg.name + ': no such file or directory');
+    }
+
+  }
 }
 
-// process the file list
+// process file arguments
 let iterator = (index) => {
 
   if (index == program.args.length) {
-
-    console.log('ALL DONE LOOPING');
-
-    // tell caller that data is processed
     callback();
     return;
   }
@@ -37,28 +79,29 @@ let iterator = (index) => {
   fs.stat(program.args[index], (err, stats) => {
 
     const path = program.args[index];
-    // console.log('PROCESS ' + path + ' AT INDEX: ' + index);
 
-    // error file.
     if (err) {
-      fileList.push({ name: path, isDir: false, isFile: false, files: null });
-      // console.log('error with ' + path);
-
+      fileList.push(createFileDataObj(path, false, false, null));
     }
 
     else if (stats.isDirectory()) {
-      const fileData = { name: path, isDir: true, isFile: false, files: null };
+
+      const fileData = createFileDataObj(path, true, false, null);
       fileData.files = fs.readdirSync(path);
+
+      fileData.files.push('.');
+      fileData.files.push('..');
+      fileData.files.sort();
+
       fileList.push(fileData);
+
     }
 
     else if (stats.isFile()) {
-      // console.log(path + ' IS A FILE');
-      fileList.push({ name: path, isDir: false, isFile: true, files: null });
-
+      fileList.push(createFileDataObj(path, false, true, null));
     }
 
-    iterator(index + 1)
+    iterator(index + 1);
 
   });
 
